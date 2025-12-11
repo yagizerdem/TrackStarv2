7c439c75cc2d9782b04ebffac4bc363c9a5bece9
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2025-12-11 19:14:47 +0300

    default layout header added

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,8 +3,8 @@ import { DefaultLayout } from "../layout/default-layout";
 export function HomePage() {
   return (
     <DefaultLayout>
-      <div className="w-full h-full  overflow-auto ">
-        <div className="h-999 w-full  "></div>
+      <div className="w-full h-full  overflow-auto text-foreground">
+        <div>hoem </div>
       </div>
     </DefaultLayout>
   );
